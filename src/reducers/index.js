b34674f47a9e63087274bb7b48dce863e9ebc050
charan51 <cvcharan51@gmail.com>
2019-03-21 18:20:52 +0530

    added redux form fields

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
-import {combineReducers} from 'redux';
-import {connectRouter} from 'connected-react-router';
-
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { reducer as formReducer } from 'redux-form';
 export default (history) => combineReducers({
     router: connectRouter(history),
+    form: formReducer
 });
