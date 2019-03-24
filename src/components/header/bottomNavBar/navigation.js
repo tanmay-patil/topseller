6406170893dc1d65a87dd017eb2cacbf290400c7
charan51 <cvcharan51@gmail.com>
2019-03-24 11:07:15 +0530

    updated header navigation names

--- src/components/header/bottomNavBar/navigation.js
+++ src/components/header/bottomNavBar/navigation.js
@@ -1,122 +1,106 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 class Navigation extends React.Component {
     render() {
         return (
             <Grid item xs={12} lg={7} sm={7} md={7}>
-                    <nav className="category">
-                        <ul className="category__menu">
-                            <li className="category__menu__item submenu">
-                                <Typography>
-                                    Men
-                                        </Typography>
-                                <ul className="megamenu">
-                                    <ul>
-                                        <h4>Clothing</h4>
-                                        <li>
-                                            <Typography>Top</Typography>
-                                        </li>
-                                        <li>
-                                            <Typography>Bottom</Typography>
+                <nav className="category">
+                    <ul className="category__menu">
+                        <li className="category__menu__item submenu">
+                            <a href="#">Men</a>
+                            <ul className="megamenu">
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <li>
+                                        <a href="#">Top</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Bottom</a>
 
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
+                                    </li>
+                                    <li>
+                                        <a href="#">Outer</a>
 
-                                        </li>
-                                    </ul>
-                                    <ul>
-                                        <h4>Clothing</h4>
-                                        <li>
-                                            <Typography>Top</Typography>
-                                        </li>
-                                        <li>
-                                            <Typography>Bottom</Typography>
+                                    </li>
+                                    <li>
+                                        <a href="#">Dresses & Onepiece</a>
 
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
+                                    </li>
+                                    <li>
+                                        <a href="#">Sports Wear</a>
 
-                                        </li>
-                                    </ul>
-                                    <ul>
-                                        <h4>Clothing</h4>
-                                        <li>
-                                            <Typography>Top</Typography>
-                                        </li>
-                                        <li>
-                                            <Typography>Bottom</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                    </ul>
-                                    <ul>
-                                        <h4>Clothing</h4>
-                                        <li>
-                                            <Typography>Top</Typography>
-                                        </li>
-                                        <li>
-                                            <Typography>Bottom</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                    </ul>
-                                    <ul>
-                                        <h4>Clothing</h4>
-                                        <li>
-                                            <Typography>Top</Typography>
-                                        </li>
-                                        <li>
-                                            <Typography>Bottom</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                        <li>
-                                            <Typography>Outer</Typography>
-
-                                        </li>
-                                    </ul>
+                                    </li>
                                 </ul>
-                            </li>
-                            <li className="category__menu__item">
-                                <Typography>
-                                    Women
-                                        </Typography>
-                            </li>
-                            <li className="category__menu__item">
-                                <Typography>
-                                    Electronics
-                                        </Typography>
-                            </li>
-                            <li className="category__menu__item">
-                                <Typography>
-                                    Home · Baby · Hobby
-                                        </Typography>
-                            </li>
-                            <li className="category__menu__item">
-                                <Typography>
-                                    Office · Industry
-                                        </Typography>
-                            </li>
-                        </ul>
-                    </nav>
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <li><a href="#">Top</a></li>
+                                    <li><a href="#">Bottom</a></li>
+                                    <li><a href="#">Outer</a></li>
+                                    <li><a href="#">Dresses & Onepiece</a></li>
+                                    <li><a href="#">Sports Wear</a></li>
+                                    <li><a href="#">Formal Wear</a></li>
+                                    <li><a href="#">Inner & Night Wear</a></li>
+                                    <li><a href="#">Ethnic Wear</a></li>
+                                    <li><a href="#">Misc. Clothing</a></li>
+                                </ul>
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <li><a href="#">Top</a></li>
+                                    <li><a href="#">Bottom</a></li>
+                                    <li><a href="#">Outer</a></li>
+                                    <li><a href="#">Dresses & Onepiece</a></li>
+                                    <li><a href="#">Sports Wear</a></li>
+                                    <li><a href="#">Formal Wear</a></li>
+                                    <li><a href="#">Inner & Night Wear</a></li>
+                                    <li><a href="#">Ethnic Wear</a></li>
+                                    <li><a href="#">Misc. Clothing</a></li>
+                                </ul>
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <li><a href="#">Top</a></li>
+                                    <li><a href="#">Bottom</a></li>
+                                    <li><a href="#">Outer</a></li>
+                                    <li><a href="#">Dresses & Onepiece</a></li>
+                                    <li><a href="#">Sports Wear</a></li>
+                                    <li><a href="#">Formal Wear</a></li>
+                                    <li><a href="#">Inner & Night Wear</a></li>
+                                    <li><a href="#">Ethnic Wear</a></li>
+                                    <li><a href="#">Misc. Clothing</a></li>
+                                </ul>
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <li>
+                                        <a href="#">Top</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Bottom</a>
+
+                                    </li>
+                                </ul>
+                            </ul>
+                        </li>
+                        <li className="category__menu__item">
+                            <a href="#">
+                                Women
+                                        </a>
+                        </li>
+                        <li className="category__menu__item">
+                            <a href="#">
+                                Electronics
+                                        </a>
+                        </li>
+                        <li className="category__menu__item">
+                            <a href="#">
+                                Home · Baby · Hobby
+                                        </a>
+                        </li>
+                        <li className="category__menu__item">
+                            <a href="#">
+                                Office · Industry
+                                        </a>
+                        </li>
+                    </ul>
+                </nav>
             </Grid>
         )
     }
