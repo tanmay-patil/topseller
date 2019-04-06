cce3792d0457ca9cebfae55116e6acaa6fe38063
charan51 <cvcharan51@gmail.com>
2019-04-06 15:50:16 +0530

    updated key prop issue

--- src/components/AccountSignUp/buyerMemberSignUp/BuyerMemberSignUp.js
+++ src/components/AccountSignUp/buyerMemberSignUp/BuyerMemberSignUp.js
@@ -16,24 +16,24 @@ class BuyerMemberSignUp extends Component {
                     <div className="flex-column">
                         <Grid item className="buyerMemberSignUpWrapper__content left" item xs={12} lg={6}>
                             <div className="buyerMemberSignUpWrapper__terms">Marked as <span>*</span> are mandatory information to fill</div>
-                            {BuyerMemberFormLeft.map(item => {
+                            {BuyerMemberFormLeft.map((item, index) => {
                                 const isArray = Array.isArray(item);
                                 const isSelectionClass = isArray ? 'selectionField' : '';
-                                const radioButtonSelectionLeft = item.length > 0 && <div className="selectionField__content"><label className="signupInputFields__label">{item[0].title}</label> <div className="subFields">{item.map(field => <div >{!field.title && <div className="subFieldsContent"> <label className="radioContainer"><span className="radioCircle"></span><Field className="radio" label={field.label} value={field.value} type={field.type} name={field.name} component="input" />{field.label}</label> </div>}</div>)}</div></div>;
+                                const radioButtonSelectionLeft = item.length > 0 && <div className="selectionField__content"><label className="signupInputFields__label">{item[0].title}</label> <div className="subFields">{item.map((field, fieldindex) => <div key={field + fieldindex}>{!field.title && <div className="subFieldsContent"> <label className="radioContainer"><span className="radioCircle"></span><Field className="radio" label={field.label} value={field.value} type={field.type} name={field.name} component="input" />{field.label}</label> </div>}</div>)}</div></div>;
                                 return (
-                                    <div className={`${isSelectionClass}`}>{!isArray && < Field label={item.label} classname={item.class} type={item.type} placeholder={item.placeholder} name={item.name} component={SignupFields} />} {radioButtonSelectionLeft} </div>
+                                    <div key={item + index} className={`${isSelectionClass}`}>{!isArray && < Field label={item.label} classname={item.class} type={item.type} placeholder={item.placeholder} name={item.name} component={SignupFields} />} {radioButtonSelectionLeft} </div>
                                 );
                             })}
                         </Grid>
 
                         <Grid item className="buyerMemberSignUpWrapper__content right" item xs={12} lg={6}>
-                            {BuyerMemberFormRight.map(item => {
+                            {BuyerMemberFormRight.map((item, index) => {
                                 const isArray = Array.isArray(item);
                                 const isSelectionClass = isArray && item.inputType === 'none' ? 'selectionField' : '';
-                                const radioButtonSelectionRight = item.length > 0 && <div className="selectionField__content"><label className="signupInputFields__label">{item[0].title}</label> <div className="subFields">{item.map(field => <div>{!field.title && <div className="subFieldsContent"><Field label={field.label} value={field.value} type={field.type} name={field.name} component='input' /><label>{field.label}</label> </div>} </div>)}</div></div>;
-                                const dropdown = <div>  {item.fieldType === 'pinCode' && <div><Field label={item.label} placeholder={item.placeholder} type={item.type} name={item.name} component={SignupFields} /></div>} {item.inputType === 'dropdown' && <div class="country"><label className="signupInputFields__label">{item.name}</label> <Field name={item.name} data={Country} valueField="value" textField="country" component={RenderDropdownList} /> </div>}</div>;
+                                const radioButtonSelectionRight = item.length > 0 && <div className="selectionField__content"><label className="signupInputFields__label">{item[0].title}</label> <div className="subFields">{item.map((field, fieldindex) => <div key={field + fieldindex}>{!field.title && <div className="subFieldsContent"><Field label={field.label} value={field.value} type={field.type} name={field.name} component='input' /><label>{field.label}</label> </div>} </div>)}</div></div>;
+                                const dropdown = <div>  {item.fieldType === 'pinCode' && <div><Field label={item.label} placeholder={item.placeholder} type={item.type} name={item.name} component={SignupFields} /></div>} {item.inputType === 'dropdown' && <div className="country"><label className="signupInputFields__label">{item.name}</label> <Field name={item.name} data={Country} valueField="value" textField="country" component={RenderDropdownList} /> </div>}</div>;
                                 return (
-                                    <div className={`${isSelectionClass}`}> {item.inputType !== 'dropdown' && !isArray && item.fieldType !== 'pinCode' && <Field label={item.label} classname={item.class} placeholder={item.placeholder} type={item.type} name={item.name} hasGSTIN={this.props.hasGSTN} component={SignupFields} />} {radioButtonSelectionRight} {dropdown}</div>
+                                    <div key={item + index} className={`${isSelectionClass}`}> {item.inputType !== 'dropdown' && !isArray && item.fieldType !== 'pinCode' && <Field label={item.label} classname={item.class} placeholder={item.placeholder} type={item.type} name={item.name} hasGSTIN={this.props.hasGSTN} component={SignupFields} />} {radioButtonSelectionRight} {dropdown}</div>
                                 );
                             })}
                         </Grid>
@@ -59,7 +59,7 @@ class BuyerMemberSignUp extends Component {
 
                     }
                     <Grid container className="formBtnContainer">
-                        <Grid item xs="12">
+                        <Grid item xs={12}>
                             <div className="submitForm" > <button type="submit">Register</button></div>
                         </Grid>
                     </Grid>
