eb1560d5d9065cac9b91cf88fb1009b00f450357
Tanmay Patil <tanmay.patil7565@gmail.com>
2019-04-07 23:40:58 +0530

    add link to vendor system

--- src/components/header/topnavbar.js
+++ src/components/header/topnavbar.js
@@ -2,17 +2,46 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import MediaQuery from 'react-responsive';
-const TopNav = (props) => {
+const TopNav = props => {
     return (
         <MediaQuery query="(min-device-width: 768px)">
             <div className="topNavBar">
-                <Grid className="container" container direction="row" justify="flex-end" alignItems="flex-end" spacing={0}>
+                <Grid
+                    className="container"
+                    container
+                    direction="row"
+                    justify="flex-end"
+                    alignItems="flex-end"
+                    spacing={0}
+                >
                     <Grid item xs={6}>
                         <div className="topNavBar__list">
-                            <NavLink className="topNavBar__list__link" to="/signout">Sign out</NavLink>
-                            <NavLink className="topNavBar__list__link" to="/signout">Download App</NavLink>
-                            <NavLink className="topNavBar__list__link" to="/signout">Sell on TopSeller</NavLink>
-                            <NavLink className="topNavBar__list__link" to="/signout">Vendor System</NavLink>
+                            <NavLink
+                                className="topNavBar__list__link"
+                                to="/signout"
+                            >
+                                Sign out
+                            </NavLink>
+                            <NavLink
+                                className="topNavBar__list__link"
+                                to="/signout"
+                            >
+                                Download App
+                            </NavLink>
+                            <NavLink
+                                className="topNavBar__list__link"
+                                to="/signout"
+                            >
+                                Sell on TopSeller
+                            </NavLink>
+                            {/* <NavLink className="topNavBar__list__link" to="/signout">Vendor System</NavLink> */}
+                            <a
+                                className="non-decorated-link"
+                                href="http://52.14.150.53:3000"
+                                target="_blank"
+                            >
+                                Vendor System
+                            </a>
                         </div>
                     </Grid>
                 </Grid>
